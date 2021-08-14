e9ff709d8c5507eb24a1564ce847548cd1385273
muhammadpauzi <muhammadpauzi200@gmail.com>
2021-08-14 21:33:43 +0700

    Apply animation and transition on todo's list

--- src/components/Todos.svelte
+++ src/components/Todos.svelte
@@ -1,12 +1,18 @@
 <script>
+    import { fade } from "svelte/transition";
+    import { flip } from "svelte/animate";
     import Todo from "./Todo.svelte";
     import todosStore from "../stores/todosStore";
 </script>
 
 {#if $todosStore.length === 0}
-    <p class="text-danger text-center py-3 fw-medium">Todos not added yet.</p>
+    <p class="text-danger text-center py-3 fw-medium" in:fade={{ delay: 500 }}>
+        Todos not added yet.
+    </p>
 {:else}
     {#each $todosStore as todo (todo.id)}
-        <Todo {todo} />
+        <div in:fade out:fade animate:flip={{ duration: 500 }}>
+            <Todo {todo} />
+        </div>
     {/each}
 {/if}
