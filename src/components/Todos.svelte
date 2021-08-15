58123001248873ef89ab5171dde20940b7a2f910
muhammadpauzi <muhammadpauzi200@gmail.com>
2021-08-15 19:25:29 +0700

    Refactoring looping todo item more simple

--- src/components/Todos.svelte
+++ src/components/Todos.svelte
@@ -5,14 +5,12 @@
     import todosStore from "../stores/todosStore";
 </script>
 
-{#if $todosStore.length === 0}
+{#each $todosStore as todo (todo.id)}
+    <div in:fade out:fade animate:flip={{ duration: 500 }}>
+        <Todo {todo} />
+    </div>
+{:else}
     <p class="text-danger text-center py-3 fw-medium" in:fade={{ delay: 500 }}>
         Todos not added yet.
     </p>
-{:else}
-    {#each $todosStore as todo (todo.id)}
-        <div in:fade out:fade animate:flip={{ duration: 500 }}>
-            <Todo {todo} />
-        </div>
-    {/each}
-{/if}
+{/each}
